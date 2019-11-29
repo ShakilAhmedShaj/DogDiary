9685490c0a7c427529411f523bf694fe952171fb
Shaj <shakilahmedshaj@gmail.com>
2019-11-29 15:55:44 +0600

    nav 1 done

--- app/src/main/java/com/shajt3ch/dogdiary/view/ListFragment.java
+++ app/src/main/java/com/shajt3ch/dogdiary/view/ListFragment.java
@@ -3,15 +3,26 @@ package com.shajt3ch.dogdiary.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.shajt3ch.dogdiary.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ListFragment extends Fragment {
+
+    @BindView(R.id.floatingActionButton)
+    FloatingActionButton fab;
 
 
     public ListFragment() {
@@ -23,7 +34,26 @@ public class ListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        fab.setOnClickListener(v -> {
+            onGotoDetails();
+        });
+    }
+
+    void onGotoDetails() {
+
+        NavDirections action = ListFragmentDirections.actionDetail();
+        Navigation.findNavController(fab).navigate(action);
+
     }
 
 
