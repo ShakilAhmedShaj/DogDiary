3281cc61d1de975b2af5c6782269339e3fc2eca3
Shaj <shakilahmedshaj@gmail.com>
2019-11-29 14:31:30 +0600

    base done

---
+++ app/src/main/java/com/shajt3ch/dogdiary/view/DetailFragment.java
@@ -0,0 +1,29 @@
+package com.shajt3ch.dogdiary.view;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.shajt3ch.dogdiary.R;
+
+public class DetailFragment extends Fragment {
+
+
+    public DetailFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/shajt3ch/dogdiary/view/ListFragment.java
@@ -0,0 +1,29 @@
+package com.shajt3ch.dogdiary.view;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.shajt3ch.dogdiary.R;
+
+public class ListFragment extends Fragment {
+
+
+    public ListFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+}
