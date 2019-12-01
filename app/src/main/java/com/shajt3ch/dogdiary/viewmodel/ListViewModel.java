a52f7b692910eac165d41811c77e12d5d1ef1cf8
Shaj <shakilahmedshaj@gmail.com>
2019-12-01 22:47:54 +0600

    api call done

--- app/src/main/java/com/shajt3ch/dogdiary/viewmodel/ListViewModel.java
+++ app/src/main/java/com/shajt3ch/dogdiary/viewmodel/ListViewModel.java
@@ -7,9 +7,14 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 
 import com.shajt3ch.dogdiary.model.DogBreed;
+import com.shajt3ch.dogdiary.model.DogsApiService;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by SHAJ on 01 Dec, 2019.
@@ -22,47 +27,52 @@ public class ListViewModel extends AndroidViewModel {
     public MutableLiveData<Boolean> dogLoadError = new MutableLiveData<Boolean>();
     public MutableLiveData<Boolean> loading = new MutableLiveData<Boolean>();
 
+    private DogsApiService dogsService = new DogsApiService();
+
+    private CompositeDisposable disposable = new CompositeDisposable();
+
 
     public ListViewModel(@NonNull Application application) {
         super(application);
     }
 
     public void refresh() {
-        DogBreed dog1 = new DogBreed("1", "Myanmar", "10 Year", "", "", "", "");
-        DogBreed dog2 = new DogBreed("2", "Indian", "15 Year", "", "", "", "");
-        DogBreed dog3 = new DogBreed("3", "Israel", "20 Year", "", "", "", "");
-        DogBreed dog4 = new DogBreed("4", "Israel", "20 Year", "", "", "", "");
-        DogBreed dog5 = new DogBreed("5", "Israel", "20 Year", "", "", "", "");
-        DogBreed dog6 = new DogBreed("6", "Israel", "20 Year", "", "", "", "");
-        DogBreed dog7 = new DogBreed("7", "Israel", "20 Year", "", "", "", "");
-        DogBreed dog8 = new DogBreed("8", "Myanmar", "10 Year", "", "", "", "");
-        DogBreed dog9 = new DogBreed("9", "Indian", "15 Year", "", "", "", "");
-        DogBreed dog10 = new DogBreed("10", "Israel", "20 Year", "", "", "", "");
-        DogBreed dog11 = new DogBreed("11", "Israel", "20 Year", "", "", "", "");
-        DogBreed dog12 = new DogBreed("12", "Israel", "20 Year", "", "", "", "");
-        DogBreed dog13 = new DogBreed("13", "Israel", "20 Year", "", "", "", "");
-        DogBreed dog14 = new DogBreed("14", "Israel", "20 Year", "", "", "", "");
+        fetchFromRemote();
 
-        ArrayList<DogBreed> dogsList = new ArrayList<>();
-        dogsList.add(dog1);
-        dogsList.add(dog2);
-        dogsList.add(dog3);
-        dogsList.add(dog4);
-        dogsList.add(dog5);
-        dogsList.add(dog6);
-        dogsList.add(dog7);
-        dogsList.add(dog8);
-        dogsList.add(dog9);
-        dogsList.add(dog10);
-        dogsList.add(dog11);
-        dogsList.add(dog12);
-        dogsList.add(dog13);
-        dogsList.add(dog14);
+    }
 
-        dogs.setValue(dogsList);
-        dogLoadError.setValue(false);
-        loading.setValue(false);
+    private void fetchFromRemote() {
+        loading.setValue(true);
 
+        disposable.add(
 
+                dogsService.getDogs()
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(new DisposableSingleObserver<List<DogBreed>>() {
+                            @Override
+                            public void onSuccess(List<DogBreed> dogBreeds) {
+
+                                dogs.setValue(dogBreeds);
+                                dogLoadError.setValue(false);
+                                loading.setValue(false);
+
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+
+                                dogLoadError.setValue(true);
+                                loading.setValue(false);
+                                e.printStackTrace();
+                            }
+                        })
+        );
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        disposable.clear();
     }
 }
