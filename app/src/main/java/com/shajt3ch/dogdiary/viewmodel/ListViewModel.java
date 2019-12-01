2a3def1611fb4fb579fdd18b1f0092668fc51e1d
Shaj <shakilahmedshaj@gmail.com>
2019-12-01 14:11:18 +0600

    mvvm done 1

--- app/src/main/java/com/shajt3ch/dogdiary/viewmodel/ListViewModel.java
+++ app/src/main/java/com/shajt3ch/dogdiary/viewmodel/ListViewModel.java
@@ -26,11 +26,33 @@ public class ListViewModel extends AndroidViewModel {
         DogBreed dog1 = new DogBreed("1", "Myanmar", "10 Year", "", "", "", "");
         DogBreed dog2 = new DogBreed("2", "Indian", "15 Year", "", "", "", "");
         DogBreed dog3 = new DogBreed("3", "Israel", "20 Year", "", "", "", "");
+        DogBreed dog4 = new DogBreed("4", "Israel", "20 Year", "", "", "", "");
+        DogBreed dog5 = new DogBreed("5", "Israel", "20 Year", "", "", "", "");
+        DogBreed dog6 = new DogBreed("6", "Israel", "20 Year", "", "", "", "");
+        DogBreed dog7 = new DogBreed("7", "Israel", "20 Year", "", "", "", "");
+        DogBreed dog8 = new DogBreed("8", "Myanmar", "10 Year", "", "", "", "");
+        DogBreed dog9 = new DogBreed("9", "Indian", "15 Year", "", "", "", "");
+        DogBreed dog10 = new DogBreed("10", "Israel", "20 Year", "", "", "", "");
+        DogBreed dog11 = new DogBreed("11", "Israel", "20 Year", "", "", "", "");
+        DogBreed dog12 = new DogBreed("12", "Israel", "20 Year", "", "", "", "");
+        DogBreed dog13 = new DogBreed("13", "Israel", "20 Year", "", "", "", "");
+        DogBreed dog14 = new DogBreed("14", "Israel", "20 Year", "", "", "", "");
 
         ArrayList<DogBreed> dogsList = new ArrayList<>();
         dogsList.add(dog1);
         dogsList.add(dog2);
         dogsList.add(dog3);
+        dogsList.add(dog4);
+        dogsList.add(dog5);
+        dogsList.add(dog6);
+        dogsList.add(dog7);
+        dogsList.add(dog8);
+        dogsList.add(dog9);
+        dogsList.add(dog10);
+        dogsList.add(dog11);
+        dogsList.add(dog12);
+        dogsList.add(dog13);
+        dogsList.add(dog14);
 
         dogs.setValue(dogsList);
         dogLoadError.setValue(false);
